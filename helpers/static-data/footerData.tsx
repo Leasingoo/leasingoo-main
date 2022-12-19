e012c6e382e2169c4826bffbb83a4c52f52c3f5d
hakimm77 <hakimboum406@gmail.com>
2022-12-19 12:47:46 +0100

    fixed footer routes

--- helpers/static-data/footerData.tsx
+++ helpers/static-data/footerData.tsx
@@ -2,14 +2,14 @@ export const privateLeasingRoutes = [
   { name: "Jämför leasing", route: "/" },
   { name: "Bilhandlare", route: "/retailers" },
   { name: "Bilmärken", route: "/car-brands" },
-  { name: "Om privatleasing", route: "/" },
-  { name: "Försäkring", route: "/" },
-  { name: "Garantier", route: "/" },
-  { name: "Elbilar", route: "/" },
+  { name: "Om privatleasing", route: "/about-privatleasing" },
+  { name: "Försäkring", route: "/about-privatleasing/insurance" },
+  { name: "Garantier", route: "/about-privatleasing/guarantees" },
+  { name: "Elbilar", route: "/about-privatleasing/electric-cars" },
 ];
 
 export const leasingooRoutes = [
-  { name: "Företagslösningar", route: "/" },
+  { name: "Företagslösningar", route: "/business-solutions" },
   { name: "Om oss", route: "/about-us" },
   { name: "Kontakta oss", route: "/contact" },
 ];
